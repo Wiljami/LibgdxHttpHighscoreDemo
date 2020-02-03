7c35d9ecc4437aa66fd63201d2c350f523b16a09
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-03 10:09:18 +0200

    Change demo UI to use stage.

--- core/src/fi/tuni/tiko/MainClass.java
+++ core/src/fi/tuni/tiko/MainClass.java
@@ -2,45 +2,86 @@ package fi.tuni.tiko;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * MainClass is just for demo purposes in this project.
  */
 public class MainClass extends ApplicationAdapter implements HighScoreListener {
-	private SpriteBatch batch;
-	private OrthographicCamera camera;
+	private Stage stage;
+	private Skin skin;
 
-	private int height = 800;
-	private int width = 600;
+	private Table content;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, width, height);
-		createFont();
+		skin = new Skin();
+		skin = new Skin (Gdx.files.internal("uiskin.json"));
 		HighScoreServer.setGetUrl("https://highscore-demo.herokuapp.com/get/");
 		HighScoreServer.setPostUrl("https://highscore-demo.herokuapp.com/add");
 		HighScoreServer.setVerbose(true);
 		HighScoreServer.fetchHighScores(this);
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		content = new Table();
+		content.setDebug(true);
+		createTable();
+		stage.addActor(content);
 	}
 
-	private void createFont() {
-		FreeTypeFontGenerator fontGenerator =
-				new FreeTypeFontGenerator(Gdx.files.internal("CheyenneSansNovus-Regular.ttf"));
-		FreeTypeFontGenerator.FreeTypeFontParameter parameter =
-				new FreeTypeFontGenerator.FreeTypeFontParameter();
-		parameter.size = 24;
-		parameter.color = Color.BLACK;
-		font = fontGenerator.generateFont(parameter);
+	private ArrayList<Label> scoreLabels;
+
+	private void updateScores(List<HighScoreEntry> scores) {
+		int i = 0;
+		for (HighScoreEntry e : scores) {
+			String entry = e.getScore() + " - " + e.getName();
+			scoreLabels.get(i).setText(entry);
+			i++;
+		}
+	}
+
+	private void createTable() {
+		content.setFillParent(true);
+		content.add(new Label("High Scores", skin)).colspan(2);
+
+		scoreLabels = new ArrayList<>();
+
+
+		for (int n = 0; n < 10; n++) {
+			content.row();
+			Label l = new Label("", skin);
+			content.add(l).colspan(2);
+			scoreLabels.add(l);
+		}
+
+		TextButton fetch = new TextButton("Fetch highscores", skin);
+		fetch.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				fetchHighScores();
+			}
+		});
+
+		TextButton newHighSCore = new TextButton("Add new highscore", skin);
+   		content.row();
+		content.add(fetch);
+		content.add(newHighSCore);
+	}
+
+	private void fetchHighScores() {
+		HighScoreServer.fetchHighScores(this);
 	}
 
 	private void sendScore() {
@@ -48,48 +89,24 @@ public class MainClass extends ApplicationAdapter implements HighScoreListener {
 		HighScoreServer.sendNewHighScore(score, this);
 	}
 
-
-	private BitmapFont font;
-
-	private String textToDraw = "";
-
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		batch.setProjectionMatrix(camera.combined);
-
-		batch.begin();
-
-		font.draw(batch, textToDraw, 20, height-50);
-
-		batch.end();
+		stage.draw();
 	}
 
 	@Override
 	public void dispose () {
-		batch.dispose();
-		font.dispose();
+		skin.dispose();
 	}
 
-	private void buildNewString(List<HighScoreEntry> scores) {
-		StringBuilder string = new StringBuilder();
-		string.append("Highscores:\n");
-		if (scores == null) {
-			textToDraw = string.toString();
-		} else {
-			for (HighScoreEntry e : scores) {
-				string.append(e.getScore()).append("  ").append(e.getName()).append("\n");
-			}
-			textToDraw = string.toString();
-		}
-	}
 
 	@Override
 	public void receiveHighScore(List<HighScoreEntry> highScores) {
 		Gdx.app.log("MainClass", "Received new high scores successfully");
-		buildNewString(highScores);
+		updateScores(highScores);
 	}
 
 	@Override
